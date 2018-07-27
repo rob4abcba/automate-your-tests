c54c2f846e869d328d24501b2f49e0b35e96898e
rob4abcba <rob_abcba@yahoo.com>
2018-07-27 15:54:05 -0700

    Add pytest to Travis

---
+++ test_sample.py
@@ -0,0 +1,17 @@
+"""Automate your Tests Tutorial:
+
+https://glenjarvis.com/interactions/automate-your-tests/
+"""
+
+
+def inc(initial_value):
+    """Return arithmetic increase of 1 to initial_value"""
+    return initial_value + 1
+
+
+def test_answer():
+    """Sample test for our repository
+
+    We expect this test to pass
+    """
+    assert inc(4) == 5
